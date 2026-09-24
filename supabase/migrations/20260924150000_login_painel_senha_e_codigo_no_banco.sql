c88284b8c8b745c57f11a9eec3afc0dc0c8f8d88
José Silva <josegabrielpo422@gmail.com>
2026-09-24 15:26:59 -0300

    Senha do painel: mínimo 8 com maiúscula, minúscula, número e especial

--- supabase/migrations/20260924150000_login_painel_senha_e_codigo_no_banco.sql
+++ supabase/migrations/20260924150000_login_painel_senha_e_codigo_no_banco.sql
@@ -129,8 +129,9 @@ create or replace function interno.definir_senha_painel(p_email text, p_senha te
 language plpgsql security definer set search_path = ''
 as $$
 begin
-  if char_length(coalesce(p_senha, '')) < 10 then
-    raise exception 'A senha precisa ter pelo menos 10 caracteres.';
+  if char_length(coalesce(p_senha, '')) < 8
+     or p_senha !~ '[A-Z]' or p_senha !~ '[a-z]' or p_senha !~ '[0-9]' or p_senha !~ '[^A-Za-z0-9]' then
+    raise exception 'A senha precisa ter no mínimo 8 caracteres, com letra maiúscula, minúscula, número e caractere especial.';
   end if;
   update interno.usuarios_internos
      set senha_hash = extensions.crypt(p_senha, extensions.gen_salt('bf', 10)), tentativas_falhas = 0, bloqueado_ate = null
